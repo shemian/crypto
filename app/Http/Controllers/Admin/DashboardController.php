f77ef644da568cc7eced3167c3f332c4b968e78a
shemian <shemian092@gmail.com>
2022-11-16 12:28:22 -0800

    fetch all the transactions on the Deposits table

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,36 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Deposit;
+use DataTables;
 
 class DashboardController extends Controller
 {
     //
 
     public function index(){
-        return view('admin/index');
+        return view('admin/dashboard');
     }
+
+    public function regUsers(){
+        return view('admin/regusers');
+    }
+
+    public function getTransactions(Request $request){
+        if($request->ajax()){
+            $transactions = Deposit::all();
+            return Datatables::of($transactions)
+            ->addIndexColumn()
+                ->addColumn('action', function($row){
+                    $actionBtn = '<a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Confirm</span></a> ';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+
+        }
+        return view('admin/transaction');
+        
+
+    } 
 }
