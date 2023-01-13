09a28c7729981f77cb1edd5295a3064fc295c115
shemian <shemian092@gmail.com>
2023-01-13 05:02:06 -0800

    update site routes

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,35 @@ Route::get('/', function () {
     return view('layouts.home');
 });
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 //user wallet group routes
 
-Route::prefix('')->middleware(['auth'])->group(function(){
+Route::prefix('')->middleware(['auth', 'verified'])->group(function(){
 
 Route::get('/wallet', [WalletController::class, 'index'])->name('wallet');
-Route::post('/wallet', [WalletController::class, 'purchase'])->name('purchase'); 
+Route::post('/wallet', [WalletController::class, 'purchase'])->name('purchase');
 Route::put('/wallet', [WalletController::class, 'update'])->name('update_user_profile');
+Route::post('/wallet/withdraw', [WalletController::class, 'withdrawrequest'])->name('withdraw_request'); 
+
+//other Routes
+Route::get('/exchange-live', [WalletController::class, 'exchange_live'])->name('exchange_live');
+Route::get('/exchange', [WalletController::class, 'exchange'])->name('exchange_price');
+Route::get('/technical-analysis', [WalletController::class, 'technical_analysis'])->name('technical_analysis');
+Route::get('/market-crypto', [WalletController::class, 'market_crypto'])->name('market_crypto');
 
 });
+
 //admin Routes
-Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function(){
+Route::prefix('admin')->middleware(['auth', 'isAdmin', 'verified'])->group(function(){
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/regusers', [DashboardController::class, 'regUsers'])->name('regusers');
     Route::get('/transactions', [DashboardController::class, 'getTransactions'])->name('transactions');
+    Route::post('/transactionDetails', [DashboardController::class, 'getTransactionDetails' ])->name('user.transaction.details');
+    Route::post('/updatetransactionDetails', [DashboardController::class, 'updateTransactionDetails' ])->name('update.transaction.details');
 
 
 });
