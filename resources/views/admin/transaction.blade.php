5c9bed8e35e9cb932fd967627ae2245082116d7d
shemian <shemian092@gmail.com>
2023-01-13 04:59:50 -0800

    Added js to update the user transaction status

--- resources/views/admin/transaction.blade.php
+++ resources/views/admin/transaction.blade.php
@@ -7,6 +7,8 @@
 
 @endsection()
 
+
+
 <div class="card shadow mb-4">
     <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
@@ -21,8 +23,9 @@
                         <th>Amount</th>
                         <th>Exchange</th>
                         <th>Transaction Code </th>
-                        <!-- <th>status</th> -->
+                        <th>status</th>
                         <th>Action</th>
+                     
                     
                     </tr>
                 </thead>
@@ -33,8 +36,9 @@
                         <th>Amount</th>
                         <th>Exchange</th>
                         <th>Transaction Code </th>
-                        <!-- <th>status</th> -->
+                        <th>status</th>
                         <th>Action</th>
+                        
                     </tr>
                 </tfoot>
                 <tbody>
@@ -45,20 +49,37 @@
     </div>
 </div>
 
+@include('admin.update')
+
 @endsection
 
+
+
 @section('scripts')
+
 <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 <script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
   $(function () {
 
+    
     var table = $('#transactions').DataTable({
+       
+
         processing: true,
         serverSide: true,
         // scrollX: true,
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
         ], //not working
+
+        
         
         ajax: {
             "url" : "/admin/transactions",
@@ -73,20 +94,67 @@
             {data: 'amount', name: 'amount'},
             {data: 'coin', name: 'coin'},
             {data: 'transaction_code', name: 'transaction_code'},
+            {data: 'status', name: 'status'},
             {
                 data: 'action', 
                 name: 'action', 
                 orderable: true, 
                 searchable: true,
             }, 
+             
         ]
-        
-
-       
     });
+
+    $(document).on('click', '#editStatus', function(){
+        var transaction_id = $(this).data('id');
+        $('.editstatus').find('form')[0].reset();
+        $('.editstatus').find('spam.error-text').text('text');
+
+        $.post('<?= route("user.transaction.details") ?>', {transaction_id}, function(data ){
+            $('.editstatus').find('input[name="transaction_id"]').val(data.details.id);
+            $('.editstatus').find('input[name="name"]').val(data.details.user_id);
+            $('.editstatus').find('input[name="amount"]').val(data.details.amount);
+            $('.editstatus').find('input[name="coin"]').val(data.details.coin);
+            $('.editstatus').find('input[name="status"]').val(data.details.status);
+            $('.editstatus').modal('show')       
+        }, 'json');
+    });
+
+    //Update Transaction status
+    $('#update-transaction-detail').on('submit', function(e){
+        e.preventDefault();
+        var form = this;
+        var status = $('#status').val();
+        var transaction_id =$('#transaction_id').val();
+        $.ajax({
+            url:$(form).attr('action'),
+            method:$(form).attr('method'),
+            data:{status: status, transaction_id: transaction_id},
+            beforeSend:function(){
+                $(form).find('span.error-text').text('')
+            },
+            success:function(data){
+                if(data.code == 0){
+                    $.each(data.error, function(prefix, val ){
+                        $(form).find('span.'+prefix+'_error').text(val[0]);
+                    });
+                }else{
+                    $('editstatus').modal('hide');
+                    $('editstatus').find('form')[0].reset();
+                    toastr.success(data.msg)
+                }
+            }
+                
+
+        })
+    })
+
+
+    
 
   });
   
 </script>
 
 @endsection()
+
