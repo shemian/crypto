900280028b3a7bf3fa12a3879e26a0b5a37a8140
shemian <shemian092@gmail.com>
2023-01-13 04:55:05 -0800

    Transaction status enumClass

---
+++ app/Enums/TransactionStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class TransactionStatus extends Enum
+{
+    const Paid =   0;
+    const Pending=   1;
+}
