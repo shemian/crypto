a301a8047adba909c48b63781e0218be9548fbcb
shemian <shemian092@gmail.com>
2023-01-13 04:57:25 -0800

    Update the status of the transaction and update in the transaction table.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Deposit;
 use DataTables;
+use Validator;
+use Log;
 
 class DashboardController extends Controller
 {
@@ -19,21 +21,66 @@ class DashboardController extends Controller
         return view('admin/regusers');
     }
 
+    //GET ALL TRANSACTIONS
+
     public function getTransactions(Request $request){
         if($request->ajax()){
             $transactions = Deposit::all();
             return Datatables::of($transactions)
             ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $actionBtn = '<a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Confirm</span></a> ';
-                    return $actionBtn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
+            
+            ->addColumn('action', function(Deposit $row){
+                $actionBtn='<button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="editStatus" data-id="'.$row['id'].'">Edit</button>';
+                return $actionBtn;
+            })
+            ->rawColumns(['action'])
+               
+            ->make(true);
 
         }
         return view('admin/transaction');
         
 
     } 
+
+
+    //GET TRANSACTION DETAILS
+
+    public function getTransactionDetails(Request $request){
+        $id = $request->transaction_id;
+        $transaction = Deposit::find ($id);
+        return response()->json(['details'=>$transaction]);
+
+
+    }
+
+
+    //Update transaction status
+
+    public function updateTransactionDetails(Request $request){
+        $transaction_id = $request->transaction_id;
+        $validator = Validator::make($request->all(),[
+            'status'=>'required::deposits,status,'.$transaction_id,
+        ]);
+
+        if(!$validator->passes()){
+            return response()->json(['code'=>0,'error'=>$validator->errors()->toArray()]);
+        }else{
+            $transaction = Deposit::find($transaction_id);
+            $transaction->status = $request->status;
+            $query = $transaction->save();
+            Log::info($query);
+            if($query){
+                return response()->json(['code'=>1, 'msg'=>'Status Updated Successfuly']);
+            }
+            else{
+                return response()->json(['code'=>0, 'msg'=>'Failed Please try again Later']);
+            }
+        }
+    }
+
+
+
+
+ 
 }
